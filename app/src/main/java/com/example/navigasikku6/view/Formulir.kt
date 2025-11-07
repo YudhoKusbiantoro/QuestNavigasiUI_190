a050c0703374959e05d827b985770022f0d37308
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-11-07 14:39:11 +0700

    Menambahkan input nama lengkap menggunakan OutlinedTextField

--- app/src/main/java/com/example/navigasikku6/view/Formulir.kt
+++ app/src/main/java/com/example/navigasikku6/view/Formulir.kt
@@ -47,5 +47,15 @@ fun FormIsian(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {},
+            )
+        }
     }
 }
