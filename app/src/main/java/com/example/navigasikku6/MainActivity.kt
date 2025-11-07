c74e57be3189943dd7a6fb8f55ffcaf2b6d0e273
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-11-07 15:10:48 +0700

    Menambahkan fungsi ke MainActivity.kt

--- app/src/main/java/com/example/navigasikku6/MainActivity.kt
+++ app/src/main/java/com/example/navigasikku6/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.navigasikku6.ui.theme.Navigasikku6Theme
+
+private var MainActivity.modifier: Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +23,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Navigasikku6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    DataApp()
                 }
             }
         }
