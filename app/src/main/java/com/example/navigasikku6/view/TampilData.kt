a35da54bfe6caced592f8dfe82309c8057fe731a
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-11-07 14:58:38 +0700

    Menampilkan data menggunakan loop items dan tampilan teks berformat

--- app/src/main/java/com/example/navigasikku6/view/TampilData.kt
+++ app/src/main/java/com/example/navigasikku6/view/TampilData.kt
@@ -44,7 +44,15 @@ fun TampilData(
         }){ isiRuang->
         Column (modifier = Modifier.padding(isiRuang),
             verticalArrangement = Arrangement.SpaceBetween) {
-
+            Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy (10.dp)) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
             }
         }
 
